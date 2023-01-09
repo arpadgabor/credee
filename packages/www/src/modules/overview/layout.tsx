2bd475bc4a04eabc9cf50437d067c0d88e953204
Arpad Gabor <hello@arpadgabor.com>
2023-01-10 00:14:29 +0200

    feat: update css layout

--- packages/www/src/modules/overview/layout.tsx
+++ packages/www/src/modules/overview/layout.tsx
@@ -1,14 +1,19 @@
 import { A, Outlet } from '@solidjs/router'
+import { cva } from 'class-variance-authority'
+import { link } from 'fs'
 import { Component } from 'solid-js'
 
 const NavLink: Component<{ href: string; label: string }> = props => {
+  const linkClass = cva([
+    'w-full py-2 px-3',
+    'font-semibold text-gray-500',
+    'transition duration-300 ease-in-out',
+    'rounded hover:bg-gray-50',
+  ])
+  const activeClass = cva('text-blue-500 hover:bg-blue-50 bg-blue-50')
+
   return (
-    <A
-      href={props.href}
-      activeClass='!text-blue-500 !border-b'
-      end
-      class='font-semibold border-b-0 border-spacing-y-4 border-blue-200 text-gray-500'
-    >
+    <A href={props.href} end activeClass={activeClass()} class={linkClass()}>
       {props.label}
     </A>
   )
@@ -16,13 +21,19 @@ const NavLink: Component<{ href: string; label: string }> = props => {
 
 const OverviewLayout: Component = () => {
   return (
-    <div>
-      <header class='px-8 py-4 border-b border-gray-100 space-x-4'>
-        <NavLink href='/overview' label='Crawl Tasks' />
-        <NavLink href='/overview/results/reddit' label='Results from Reddit' />
-      </header>
+    <div class='w-screen max-h-screen flex'>
+      <aside class='flex flex-col w-80 border-r border-gray-200'>
+        <div class='py-6 px-6 border-b border-gray-200'>
+          <h1 class='font-bold text-2xl text-blue-700'>Credee</h1>
+        </div>
 
-      <main class='px-8 py-4'>
+        <nav class='flex flex-col space-y-2 px-4 py-4'>
+          <NavLink href='/overview' label='Crawl Tasks' />
+          <NavLink href='/overview/results/reddit' label='Results from Reddit' />
+        </nav>
+      </aside>
+
+      <main class='flex-1 px-4 py-4 overflow-y-auto'>
         <Outlet></Outlet>
       </main>
     </div>
