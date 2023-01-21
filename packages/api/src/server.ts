ae0b45518783d940e03e74e933fea4039329e5ee
Arpad Gabor <hello@arpadgabor.com>
2023-01-21 22:36:19 +0200

    feat: add static

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,14 +1,33 @@
-import { fastify } from 'fastify'
 import cors from '@fastify/cors'
-
+import { fastifyStatic } from '@fastify/static'
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify'
-import { appRouter } from './router.js'
+import { fastify } from 'fastify'
+import { resolve } from 'node:path'
 import { config } from './config.js'
 import { migrateToLatest } from './database/migrations.js'
+import { appRouter } from './router.js'
+
+const logsByEnv = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+        colorize: true,
+      },
+    },
+  },
+  production: true,
+}
 
 const server = fastify({
   maxParamLength: 5000,
-  logger: true,
+  logger: logsByEnv[config.get('env')] || true,
+})
+server.register(fastifyStatic, {
+  root: resolve(process.cwd(), 'uploads'),
+  prefix: '/uploads/',
 })
 
 server.register(cors, {
